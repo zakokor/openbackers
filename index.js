568437df72532c219dc2c73e9acc165df81a06d2
Gonza <zakokor@zakokor-MBA.local>
2020-10-04 18:07:42 -0500

    update main route

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function getRandFile() {
   return img;
 }
 
+server.get('/', (req, res) => { res.send("Hello World!")})
 
 server.get('/banner', function (req, res, next) {
   const now = new Date()
